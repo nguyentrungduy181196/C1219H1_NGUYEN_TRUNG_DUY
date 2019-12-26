258802a6bf1f55c9f521d28fa8d38bf05364c25a
nguyentrungduy181196 <nguyentrungduy181196@gmail.com>
2019-12-26 10:44:54 +0700

    update

---
+++ B2_BienKieuDuLieuvaToanTu/ThucHanh/SuDungToanTu/toantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let  inputheigh;
+ inputwidth = prompt("inter the width");
+ inputheigh= prompt("inter the height");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheigh);
+let area  = width * height;
+document.write("the area is: "+ area);
