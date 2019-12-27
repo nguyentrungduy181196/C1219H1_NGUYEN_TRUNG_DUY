9c48a3a7a24db63a6ecae16143cfcd274b8eff04
nguyentrungduy181196 <nguyentrungduy181196@gmail.com>
2019-12-27 08:03:20 +0700

    update

---
+++ B2_BienKieuDuLieuvaToanTu/BaiTap/BienKieuDuLieuVaToanTu/diem.js
@@ -0,0 +1,6 @@
+let diemtoan =parseFloat(prompt("nhap diem toan: ")) ;
+let diemly =parseFloat(prompt("nhap diem ly: ")) ;
+let diemhoa =parseFloat( prompt("nhap diem hoa"));
+        
+let diemtb =(diemtoan+diemly+diemhoa)/3;
+        document.write(diemtb)
