e08013c7f8aab2730c2a316688946bf82a893853
nguyentrungduy181196 <nguyentrungduy181196@gmail.com>
2020-01-05 22:50:20 +0700

    update

--- B4_CauTrucLap/Baitap/HienThiSoNguyentoDauTien/soN.js
+++ B4_CauTrucLap/Baitap/HienThiSoNguyentoDauTien/soN.js
@@ -1,17 +1,23 @@
-let  number = prompt("hãy nhập số :)): ");
-let count = 0;
-let  N = 2;
-for (N = 2; N <= 20; N++ ) {
-    if (number & N== 0){
-        document.write(number + ":la so nguyen to");
-        count++;
-    }
-    else {
-        document.write("khong phai la so nguyen to")
+
+    let number = parseInt(prompt('Enter your number: '));
+    let countFor = 0;
+    let countIf = 0 ;
+    if (number <= 0) {
+        alert('Khong ton tai bat ky so nguyen to nao !!!');
+    } else {
+    let n=2;
+    while (countFor<number) {
+        for ( let i=2 ; i <= n/2; i++){
+            if ( n % i === 0 ) {
+                countIf += 1;
+                break;
+            }
+        }
+        if ( countIf === 0) {
+            document.write(n + " ");
+            countFor += 1;
+        }
+        countIf=0;
+        n++;
     }
 }
-
-
-
-
-
